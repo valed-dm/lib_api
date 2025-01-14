ccd9cd4dcebad6a7bc30db40f8b3204b5e36ce82
valed-dm <dmvaled@gmail.com>
2025-01-14 14:56:10 +0300

    env.py migration steps.

--- migrations/env.py
+++ migrations/env.py
@@ -2,10 +2,19 @@ from logging.config import fileConfig
 
 from alembic import context
 from sqlalchemy import Connection
+from sqlalchemy import MetaData
 from sqlalchemy import pool
 from sqlalchemy.ext.asyncio import async_engine_from_config
 
-from db.base import Base
+from db.associations import BookAuthorAssociation
+from db.associations import BookCategoryAssociation
+from db.authors import Author
+from db.books import Book
+from db.categories import Category
+from db.images import Image
+from db.library import Library
+from db.readers import Reader
+from db.users import User
 
 # Get alembic config
 config = context.config
@@ -13,7 +22,17 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-target_metadata = Base.metadata
+# Create a temporary metadata object for the authors table
+target_metadata = MetaData()
+User.metadata.tables["users"].tometadata(target_metadata)
+Author.metadata.tables["authors"].tometadata(target_metadata)
+Category.metadata.tables["categories"].tometadata(target_metadata)
+Book.metadata.tables["books"].tometadata(target_metadata)
+Image.metadata.tables["images"].tometadata(target_metadata)
+BookAuthorAssociation.metadata.tables["book_author"].tometadata(target_metadata)
+BookCategoryAssociation.metadata.tables["book_category"].tometadata(target_metadata)
+Library.metadata.tables["libraries"].tometadata(target_metadata)
+Reader.metadata.tables["readers"].tometadata(target_metadata)
 
 
 def run_migrations_offline():
