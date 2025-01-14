dd52f37e9192cbdb13e2883ef17983ce074a2fe8
valed-dm <dmvaled@gmail.com>
2025-01-15 00:36:00 +0300

    TimestampMixin added.

---
+++ db/timestamp.py
@@ -0,0 +1,20 @@
+from datetime import datetime
+
+from sqlalchemy import DateTime
+from sqlalchemy import func
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
+
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        nullable=False,
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+        server_default=func.now(),
+        onupdate=func.now(),
+        nullable=False,
+    )
