167d20ea0a5299cfb6457d3c219fddee7cc00b21
valed-dm <dmvaled@gmail.com>
2025-01-15 23:24:17 +0300

    users.py docs revised to short.

--- db/users.py
+++ db/users.py
@@ -16,26 +16,11 @@ class User(Base, TimestampMixin):
     """
     User model for database interaction.
 
-    This model represents a user entity within the application and contains the
-    following fields:
-
-    - id: The unique identifier for the user (Primary Key).
-    - username: The user's unique username (indexed and required).
-    - email: The user's email address (unique, default to None).
-    - hashed_password: The user's password hashed for security.
-    - full_name: The user's full name (default to None).
-    - disabled: A boolean flag indicating whether the user's account is disabled.
-    - scopes: A string representing the user's permission scopes.
-    Default to empty string.
-
-    This model is designed to interact with a SQLAlchemy database using
-    asynchronous sessions.
-
     Attributes:
         id (int): The unique identifier for the user.
         username (str): The unique username associated with the user.
         email (str | None): The user's email address. Default to None.
-        hashed_password (str): The user's hashed password.
+        hashed_password (str): The user's hashed password. Required.
         full_name (str | None): The user's full name. Default to None.
         disabled (bool): Whether the user is disabled or not. Default to False.
         scopes (str): A space-separated string of the user's permissions.
