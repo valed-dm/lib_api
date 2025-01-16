c13d4968b230f4f85f7e6e68c2f9510d786c4ca8
valed-dm <dmvaled@gmail.com>
2025-01-16 14:02:28 +0300

    'get books' path added to main.py;
    docs for 'post books' and get books created.

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from fastapi import HTTPException
 from fastapi import Security
 from fastapi import status
 from fastapi.security import OAuth2PasswordRequestForm
+from pydantic import ValidationError
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from auth.auth import authenticate_user
@@ -17,6 +18,9 @@ from auth.auth import create_access_token
 from auth.token_schema import Token
 from book.book import BookCreate
 from book.create import create_book
+from book.get import get_books
+from book.read import BookRead
+from book.search import BookSearchSchema
 from db.utils import get_db
 from user.create import create_user
 from user.get import get_current_active_user
@@ -117,7 +121,105 @@ async def create_book_route(
     db: Annotated[AsyncSession, Depends(get_db)],
     _: Annotated[User, Security(get_current_active_user, scopes=["create"])],
 ):
+    """
+    Create a new book.
+
+    This endpoint allows creating a new book by providing required details such as
+    title, description, authors, categories, and other book-related information.
+
+    **Request Body:**
+    - **title** (`str`, required): The title of the book. (max length: 255)
+    - **description** (`str`, optional): A short description of the book.
+    - **date** (`date`, optional): The publication date of the book (optional).
+    - **google_book_id** (`str`, optional): The Google Books API ID (optional).
+    - **authors** (`List[str]`, required): A list of author names for a book.
+    - **categories** (`List[str]`, required): A list of category names for a book.
+    - **image_src** (`str`, optional): URL of an image for the book.
+
+    **Request Example:**
+
+    ```json
+    {
+      "title": "The Great Python",
+      "description": "An in-depth guide to Python programming.",
+      "date": "2025-03-01",
+      "google_book_id": "google-id-1234",
+      "authors": ["John Doe", "Jane Doe"],
+      "categories": ["Programming", "Python"],
+      "image_src": "https://example.com/book-image.jpg"
+    }
+    ```
+
+    **Responses:**
+    - **201 Created**: The book was successfully created.
+        - Returns the book object in the response body.
+
+    **Response Body (201):**
+    ```json
+    {
+      "id": 3,
+      "title": "The Great Python",
+      "description": "An in-depth guide to Python programming.",
+      "date": "2025-03-01",
+      "google_book_id": "google-id-1234",
+      "created_at": "2025-03-01T00:00:00Z",
+      "updated_at": "2025-03-01T00:00:00Z",
+      "authors": ["John Doe", "Jane Doe"],
+      "categories": ["Programming", "Python"]
+    }
+    ```
+
+    - **400 Bad Request**: The request data is invalid or incomplete.
+        - Returns an error message describing what went wrong.
+
+    **Raises**:
+    - **ValidationError**: If the book data is not valid or violates schema rules
+    (e.g., required fields missing).
+    - **HTTPException**: If an error occurs while creating the book in the database.
+
+    This endpoint only allows users with the `"create"` scope to access it.
+
+    **Security**:
+    - Only authenticated users with the `create` permission can create a book.
+    Unauthorized access will return a `401 Unauthorized` response.
+    """
     return await create_book(db, book_data)
+
+
+@app.get("/books/", response_model=list[BookRead], status_code=200)
+async def get_books_endpoint(
+    search_args: Annotated[BookSearchSchema, Depends(BookSearchSchema)],
+    db: Annotated[AsyncSession, Depends(get_db)],
+    _: Annotated[User, Security(get_current_active_user, scopes=["read"])],
+):
+    """
+    Get a list of books with optional filtering, sorting, and pagination.
+
+    Query Parameters:
+    - **title** (`str`, optional): Search for books by a partial match on the title.
+    - **author** (`str`, optional): Search for books by a partial match on the name.
+    - **category** (`str`, optional): Search for books by a partial match on the name.
+    - **sort_by** (`str`, default=`"date"`): Sort by (`"date"` or `"title"`).
+    - **order** (`str`, default=`"desc"`): Sorting order, either `"asc"` or `"desc"`.
+    - **limit** (`int`, default=`10`): Number of books per page.
+    - **offset** (`int`, default=`0`): Number of records to skip for pagination.
+
+        Responses:
+        - **200 OK**: A list of books matching the criteria.
+        - **400 Bad Request**: Invalid input or query parameters.
+        - **401 Unauthorized**: User authentication is required.
+        - **403 Forbidden**: The user lacks necessary scopes to access this resource.
+
+        Returns:
+        - `list[BookRead]`: A list of books matching the search and filtering criteria.
+    """
+    try:
+        books = await get_books(db=db, **search_args.model_dump(exclude_none=True))
+
+    except ValidationError as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
+    else:
+        return books
 
 
 @app.get("/users/me/", response_model=User)
