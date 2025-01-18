0c3381f29e8fd1b7eba371049a11097876bd3ac7
valed-dm <dmvaled@gmail.com>
2025-01-19 01:36:48 +0300

    /users/me/items/ endpoint created.
    returns all borrowed books for current user.

--- routes/auth/me.py
+++ routes/auth/me.py
@@ -1,8 +1,12 @@
 from typing import Annotated
 
 from fastapi import APIRouter
+from fastapi import Depends
 from fastapi import Security
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from db.utils import get_db
+from library.me_items import get_me_borrowed_books
 from user.get import get_current_active_user
 from user.user import User
 
@@ -14,3 +18,14 @@ async def read_users_me(
     current_user: Annotated[User, Security(get_current_active_user, scopes=["me"])],
 ):
     return current_user
+
+
+@user_me_router.get("/users/me/items/")
+async def get_user_borrowed_books(
+    db: Annotated[AsyncSession, Depends(get_db)],
+    current_user: Annotated[
+        User,
+        Security(get_current_active_user, scopes=["read"]),
+    ],
+):
+    return await get_me_borrowed_books(db, current_user)
