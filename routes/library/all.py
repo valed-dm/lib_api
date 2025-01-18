989e64b80396374ad39ca384940de189009ff045
valed-dm <dmvaled@gmail.com>
2025-01-18 17:30:10 +0300

    'lend_books' endpoint added.

--- routes/library/all.py
+++ routes/library/all.py
@@ -4,12 +4,15 @@ from typing import Annotated
 
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi import HTTPException
 from fastapi import Security
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from db import User
 from db.utils import get_db
 from library.add import intake_books
+from library.lend import lend_books_to_user
+from library.schemas import LendingDataList
 from library.schemas import LibraryCreate
 from library.schemas import LibraryRead
 from user.get import get_current_active_user
@@ -21,7 +24,7 @@ library_router = APIRouter(prefix="/library", tags=["Library"])
 async def library_intake(
     intake_data: list[LibraryCreate],
     db: Annotated[AsyncSession, Depends(get_db)],
-    _: Annotated[User, Security(get_current_active_user, scopes=["manage_library"])],
+    _: Annotated[User, Security(get_current_active_user, scopes=["librarian"])],
 ):
     """
     Add or update books in the library.
@@ -29,3 +32,21 @@ async def library_intake(
     - If the book does not exist, create a new entry.
     """
     return await intake_books(db, [book.model_dump() for book in intake_data])
+
+
+@library_router.post("/lend-books/", status_code=200)
+async def lend_books(
+    data: LendingDataList,
+    db: Annotated[AsyncSession, Depends(get_db)],
+    _: Annotated[
+        User,
+        Security(get_current_active_user, scopes=["librarian"]),
+    ],
+):
+    """
+    Lend books to a user.
+    """
+    try:
+        return await lend_books_to_user(db, data.lending_data)
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e)) from e
