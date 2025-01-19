f980f3e493f456391efe907daa6097f50bae30f5
valed-dm <dmvaled@gmail.com>
2025-01-19 12:44:26 +0300

    base.py got imports from sqlalchemy.orm.

--- db/base.py
+++ db/base.py
@@ -1,4 +1,4 @@
-from sqlalchemy.ext.declarative import DeclarativeMeta
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import DeclarativeMeta
+from sqlalchemy.orm import declarative_base
 
 Base: DeclarativeMeta = declarative_base()
